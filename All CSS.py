d858ba17d88156e3aba0a0555d4c4b6fef8f3471
Sunil Jarotia <imsunilkj@gmail.com>
2020-02-16 08:59:29 +0530

    Files added
    Nothing much

---
+++ All CSS.py
@@ -0,0 +1,33 @@
+### All CSS Classes ###
+### Tab Index left =                "app _1Jzz1 two"
+### Tab Index Right =               "_1_keJ"
+### Contact box =                   "_2WP9Q"
+    ### Upper Line 
+        ### Exact Contact Name =    "_3H4MS"
+        ### Message Time =          "_0LqQ"
+    ### Lower Line                  "xD91K"
+        ### Last message Preview    "_2Bw3Q"
+### Last Message
+### Chatbox Display name
+### Live Chat box
+### First message
+### Second Message
+### Third Message
+###
+###
+### time.sleep(.02)
+### time.sleep(.1)
+#######################
+
+
+#######################
+### Refresh Driver ###
+### Get index html ###
+### Save the ducument as whole (for backup) ###
+### Get specified data  ###
+### Update table (By time && our specific data need ) ###
+### specific data need (Verification Rule Needed) ###
+###     Phone Number  ###
+###     Email Address ###
+###     Name  ###
+#######################
