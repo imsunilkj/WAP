caa3e03c91d285763444372198420fc51466d56d
Sunil Jarotia <imsunilkj@gmail.com>
2020-02-20 23:56:28 +0530

    Got required data
    Got contact name, Chat box contact name, Chat Box as list

--- All CSS.py
+++ All CSS.py
@@ -1,33 +1,45 @@
-### All CSS Classes ###
-### Tab Index left =                "app _1Jzz1 two"
-### Tab Index Right =               "_1_keJ"
-### Contact box =                   "_2WP9Q"
-    ### Upper Line 
-        ### Exact Contact Name =    "_3H4MS"
-        ### Message Time =          "_0LqQ"
-    ### Lower Line                  "xD91K"
-        ### Last message Preview    "_2Bw3Q"
-### Last Message
-### Chatbox Display name
-### Live Chat box
-### First message
-### Second Message
-### Third Message
-###
-###
-### time.sleep(.02)
-### time.sleep(.1)
-#######################
+def hello():
+    print("Hello World")
 
 
-#######################
-### Refresh Driver ###
-### Get index html ###
-### Save the ducument as whole (for backup) ###
-### Get specified data  ###
-### Update table (By time && our specific data need ) ###
-### specific data need (Verification Rule Needed) ###
-###     Phone Number  ###
-###     Email Address ###
-###     Name  ###
-#######################
+# ## All CSS Classes ###
+# ## Tab Index left =                "app _1Jzz1 two"
+# ## Tab Index Right =               "_1_keJ"
+# ## Contact box =                   "_2WP9Q"
+#     ## Upper Line
+#         ## Exact Contact Name =    "_3H4MS"
+#         ## Message Time =          "_0LqQ"
+#     ## Lower Line                  "xD91K"
+#         ## Last message Preview    "_2Bw3Q"
+# ## Last Message
+# ## Chatbox Display name
+# ## Live Chat box
+# ## First message
+# ## Second Message
+# ## Third Message
+# ##
+# ##
+# ## time.sleep(.02)
+# ## time.sleep(.1)
+# ######################
+#
+#
+# ######################
+# ## Refresh Driver ###
+# ## Get index html ###
+# ## Save the ducument as whole (for backup) ###
+# ## Get specified data  ###
+# ## Update table (By time && our specific data need ) ###
+# ## specific data need (Verification Rule Needed) ###
+# ##     Phone Number  ###
+# ##     Email Address ###
+# ##     Name  ###
+# ######################
+#
+#
+# WhatsApp Web Automation
+# Click + Get Data + Scroll Down
+# References
+# https://www.geekyhub.in/post/making-a-whatsapp-bot-using-python-selenium
+# https://selenium.dev/documentation/en/getting_started_with_webdriver/performing_actions_on_the_aut/
+# http://ashishkhan.com/blog/buddy-whatsapp-chat-bot-with-python
--- Notes.py
+++ Notes.py
@@ -1,1 +1,45 @@
-##
+def get_full_contactbox_info(contact_box):
+    ### Details is information dictionary to return
+    # detail = {}
+    
+    ### Getting exact contact by CSS 
+    temp = contact_box.find_element_by_class_name("_3H4MS")
+    cnt = str(temp.text) 
+    # print(cnt)
+    ######
+
+    ### Getting first messge preview from contact box - _1Wn_k _19RFN _1ovWX _F7Vk
+    temp = contact_box.find_element_by_class_name("_1Wn_k")
+    fst_msg_box = str(temp.text)
+    # print(fst_msg_box)
+
+    thisdict = {
+    "contact": cnt,
+    "msg_preview": fst_msg_box,  
+    }
+    
+    return thisdict
+############################################################################################################
+############################################################################################################
+def get_full_chatbox_info(chat_box):
+    ### Getting exact contact name with _3V5x5
+    contact_name = chat_box.find_element_by_class_name("_3V5x5")
+    name = contact_name.text
+    
+    ### Getting full chat with _1ays2 ###
+    rawchatdata = ((chat_box.find_element_by_class_name("_1ays2")).text).split("\n")
+    
+    ### Running loop on last 5 text messages 
+    ### TODO Doing too
+    ### Cleaning chat ###
+
+    chatlist = clutfree(rawchatdata)
+    print(temp)
+####################################
+####################################
+# test = driver.find_element_by_class_name("NuujD") 
+# contact_details = get_full_chatbox_info(test)
+
+############################################################################################################
+############################################################################################################
+
